16ee62ca2fb0fe8bb461b2ea6e275b24d94fc697
Eros Cedeño <16-10216@usb.ve>
2023-04-11 00:04:02 -0400

    RBF aproximador universal

--- RBF/UniAprox.py
+++ RBF/UniAprox.py
@@ -1,12 +1,49 @@
 """
 Aproximador universal usando como base funciones de base radial.
 Se reciben como parametros:
--
+- inputs: matriz de estimulos de entrenamiento
+- expected: Vector de valores esperados por cada estimulo
+- function: funcion de base radial a aplicar a cada estimulo para formar las neuronas
+- factor_regulation: Es el parametro de regularizacion
 
 Se pueden acceder a los metodos:
 - 
 """
-import numpy
+import numpy as np
 class UniAprox:
-    def __init__(self) -> None:
-        self.G = 
+    def __init__(self, inputs, expected, function, factor_regulation = 0) -> None:
+        ( num_neuron, num_dimention ) = inputs.shape
+        self.num_neuron = num_neuron
+        self.num_dimention = num_dimention
+        self.inputs = inputs
+        self.factor = factor_regulation
+        self.function = function
+
+        print(f"Numero de Neuronas: {num_neuron}, Dimensiones: {num_dimention}\n")
+
+        G = np.zeros(( num_neuron, num_neuron ))
+        for i in range(num_neuron):
+            for j in range(num_neuron):
+                G[i][j] = function(self.distance(inputs[i], inputs[j]))
+        self.G = G
+
+        self.W = None
+        try:
+            self.W = np.linalg.solve( G + self.factor*np.identity(num_neuron), expected )
+        except:
+            raise "Error al resolver el sistema lineal"
+
+    def get_weights(self):
+        return self.W
+    
+    def distance(self, np_vector1, np_vector2) -> float:
+        return np.linalg.norm(np_vector1 - np_vector2)
+    
+    def interpolate( self, input ):
+        phi = np.zeros((self.num_neuron))
+        for i in range(self.num_neuron):
+            phi[i] = self.function(self.distance(input, self.inputs[i]))
+        return np.dot( phi, self.W )[0]
+    
+
+        
---
+++ RBF/client.py
@@ -0,0 +1,44 @@
+from UniAprox import *
+from RBF import *
+import statistics as stat
+import csv
+import matplotlib.pyplot as plt
+
+dev = -1
+def get_training_set( filename ):
+    with open(filename, 'r', encoding='utf-8') as f:
+        csvreader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+        lines = []
+        for row in csvreader:
+            lines.append(row)
+        lines = np.array(lines)
+        inputs = lines[:, [0]]
+        expected = lines[:, [1]]
+        stddev = stat.stdev(lines[:, 0])
+        return (inputs, expected, stddev)
+    
+def gaussian(x):
+    return np.exp( -1/(2*dev**2) * x )
+
+def main():
+    print("Programa CLiente Iniciado")
+    ( inputs, expected, dev )   = get_training_set( "../datos/Spectra100 - Spectra100.csv" )
+    uni = UniAprox( inputs, expected, gaussian, 0.09 )
+    x = list(inputs.T[0]) + list(np.arange( 0, 6, 0.08))
+    x.sort()
+    y = []
+    for input in x:
+        y.append( uni.interpolate( [ input ] ) )
+
+    plt.plot(x,y)
+    plt.plot(inputs.T[0], expected, 'ro')
+    plt.axis([0.09, 5, 0, 1.3] )
+    print("Graficando Curva interpoladora y datos")
+    plt.show()
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
